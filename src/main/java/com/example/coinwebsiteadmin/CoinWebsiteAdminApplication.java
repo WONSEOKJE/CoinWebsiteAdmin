6f8f83a142c8fbdbe1739e92334e572d02dcf26b
WONSEOKJE <nicholas4744@gmail.com>
2022-12-20 19:09:55 +0900

    conf:스프링부트 실행 자바파일 추가

---
+++ src/main/java/com/example/coinwebsiteadmin/CoinWebsiteAdminApplication.java
@@ -0,0 +1,13 @@
+package com.example.coinwebsiteadmin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CoinWebsiteAdminApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CoinWebsiteAdminApplication.class, args);
+    }
+
+}
