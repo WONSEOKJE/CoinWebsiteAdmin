c4cef55df91a90a8e4f7bfa972a70bcaa395cb5c
WONSEOKJE <nicholas4744@gmail.com>
2022-12-23 13:10:51 +0900

    conf:테스트 실행파일 추가

---
+++ src/test/java/com/example/coinwebsiteadmin/CoinWebsiteAdminApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.coinwebsiteadmin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CoinWebsiteAdminApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
